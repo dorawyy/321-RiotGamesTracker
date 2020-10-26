a9ca4f1aed41e98dd025536a558cb3931f21d403
unknown <victor_hyde@outlook.com>
2020-10-25 21:38:47 -0400

    notifications backend

--- backend/server.js
+++ backend/server.js
@@ -153,4 +153,34 @@ var server = app.listen(process.env.port||port, function () {
 
 })
 
+var admin = require("firebase-admin");
 
+var path = require('path');
+var serviceAccount = require( path.resolve( __dirname, "riot-games-tracker-firebase-adminsdk-5r6sl-acc118704e.json" ) );
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://riot-games-tracker.firebaseio.com"
+});
+
+function sendNotification(title, body) {
+    console.log('HERE')
+    var message = {
+      notification:{
+          title:title,
+          body:body
+        },
+      topic: "notifications"
+    };
+
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    admin.messaging().send(message)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
+}
