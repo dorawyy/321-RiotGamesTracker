5d8859a4b43ef7db5d54fcee02c2d0a074edb4cd
Tim1520 <timdegerness@gmail.com>
2020-10-31 18:23:43 -0700

    Add getters to match history

--- frontend/app/src/main/java/com/example/riotgamestracker/models/MatchHistory.java
+++ frontend/app/src/main/java/com/example/riotgamestracker/models/MatchHistory.java
@@ -12,7 +12,9 @@ import java.util.Map;
 
 public class MatchHistory {
     // Index : (Character, win)
+    @Deprecated
     public Map<String, Pair<String, Boolean>> history;
+
     private JSONObject histJson;
     private int size;
 
@@ -58,6 +60,96 @@ public class MatchHistory {
         return retVal;
     }
 
+    public int getKills(int index) {
+        int retVal = -1;
+
+        if (this.histJson != null) {
+            try {
+                retVal = this.histJson.getJSONObject("kills").getInt(Integer.toString(index));
+
+            } catch (JSONException exception) {
+                Log.d("Error", "getKills: " + exception.getMessage());
+            }
+        }
+
+        return retVal;
+    }
+
+    public int getDeaths(int index) {
+        int retVal = -1;
+
+        if (this.histJson != null) {
+            try {
+                retVal = this.histJson.getJSONObject("deaths").getInt(Integer.toString(index));
+
+            } catch (JSONException exception) {
+                Log.d("Error", "getDeaths: " + exception.getMessage());
+            }
+        }
+
+        return retVal;
+    }
+
+    public int getChampLevel(int index) {
+        int retVal = -1;
+
+        if (this.histJson != null) {
+            try {
+                retVal = this.histJson.getJSONObject("champLevel").getInt(Integer.toString(index));
+
+            } catch (JSONException exception) {
+                Log.d("Error", "getChampLevel: " + exception.getMessage());
+            }
+        }
+
+        return retVal;
+    }
+
+    public int getTotalDamageDealt(int index) {
+        int retVal = -1;
+
+        if (this.histJson != null) {
+            try {
+                retVal = this.histJson.getJSONObject("totalDamageDealt").getInt(Integer.toString(index));
+
+            } catch (JSONException exception) {
+                Log.d("Error", "getTotalDamageDealt: " + exception.getMessage());
+            }
+        }
+
+        return retVal;
+    }
+
+    public int getAssists(int index) {
+        int retVal = -1;
+
+        if (this.histJson != null) {
+            try {
+                retVal = this.histJson.getJSONObject("assists").getInt(Integer.toString(index));
+
+            } catch (JSONException exception) {
+                Log.d("Error", "getAssists: " + exception.getMessage());
+            }
+        }
+
+        return retVal;
+    }
+
+    public int getGoldEarned(int index) {
+        int retVal = -1;
+
+        if (this.histJson != null) {
+            try {
+                retVal = this.histJson.getJSONObject("goldEarned").getInt(Integer.toString(index));
+
+            } catch (JSONException exception) {
+                Log.d("Error", "getGoldEarned: " + exception.getMessage());
+            }
+        }
+
+        return retVal;
+    }
+
     public Boolean checkWin(int index) {
         Boolean retVal = null;
 
@@ -66,7 +158,7 @@ public class MatchHistory {
                 retVal = this.histJson.getJSONObject("win").getBoolean(Integer.toString(index));
 
             } catch (JSONException exception) {
-                Log.d("Error", "getCharacter: " + exception.getMessage());
+                Log.d("Error", "checkWin: " + exception.getMessage());
             }
         }
 
