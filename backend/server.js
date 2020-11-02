3c24a82fac41a06c0cfc485701c34af0c4994546
bisht10 <bishtayush0@gmail.com>
2020-11-02 01:19:26 -0800

    tut

--- backend/server.js
+++ backend/server.js
@@ -13,35 +13,35 @@ mongoose.Promise = global.Promise;
 
 app.use(express.json());
 
-const port = 8081;
+const port = 3000;    //8081
 
 app.get('/summoner', (req, res) => {
     
-    // Save Player Data in DB
-    Player.create(res);
+  // Save Player Data in DB
+  Player.create(res);
 
-    var dataToSend = "";
+  var dataToSend = "";
 
-    let name = req.query.name
+  let name = req.query.name
 
-    const python = spawn('python', ['PythonCode\\SummonerSearchDemo.py', name, "summoner"]);
+  const python = spawn('python', ['PythonCode\\SummonerSearchDemo.py', name, "summoner"]);
 
-    // spawn new child process to call the python script
-    // collect data from script
-    python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
-        dataToSend += data.toString();
-    });
-    python.stderr.on('data', function (data) {
-        console.log('Python script errored');
-            dataToSend += data.toString();
-    });
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        res.send(dataToSend)
-    });
+  // spawn new child process to call the python script
+  // collect data from script
+  python.stdout.on('data', function (data) {
+  console.log('Pipe data from python script ...');
+      dataToSend += data.toString();
+  });
+  python.stderr.on('data', function (data) {
+      console.log('Python script errored');
+          dataToSend += data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on('close', (code) => {
+      console.log(`child process close all stdio with code ${code}`);
+      // send data to browser
+      res.send(dataToSend)
+  });
 
 })
 
--- backend/tests/server.test.js
+++ backend/tests/server.test.js
@@ -1,19 +1,42 @@
-const supertest = require('supertest')
-const app = require('../server.js')
-const request = supertest(app)
+const Controller = require('../server');
 
-describe('GET', function(){
+const getSpy = jest.fn();
+
+jest.doMock('express', () => {
+  return {
+    Router() {
+      return {
+        get: getSpy,
+      }
+    }
+  }
+});
+
+describe('should test router', () => {
+  require('../server.js');
+  test('should test get POSTS', () => {
+    expect(getSpy).toHaveBeenNthCalledWith('/summoner');
+  });
+});
+
+
+/* const express = require('express')
+const app_express = express()
+jest.mock('express');
+
+describe('GET', () => {
 
     it('should return successfully', async done =>{
-        const response = await request.get('/summoner')
-        expect(response.status).toBe(200)
-        done()
-        })
+        
+        const response = {data : {id: 123}}
+        app_express.get.mockResolvedValue(response)
 
-    it('should return successfully', async done =>{
-        const response = await request.get('/profile')
-        expect(response.status).toBe(200)
+        const app = require('../server.js')
+
+        app.get('/summoner')
         done()
         })
 
 })
+
+ */
