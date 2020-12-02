f731680424a0e24cc35aafbf033577029d5d52d5
bisht10 <bishtayush0@gmail.com>
2020-12-01 16:06:50 -0800

    follow working

--- backend/PythonCode/config.py
+++ backend/PythonCode/config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 #This File should be in the .gitignore as to not leak the api key once the project is complete
 
-riot_games_api_key = "RGAPI-ef620fff-9c73-486c-bb57-79c1f06bcbcf"
+riot_games_api_key = "RGAPI-9439bfd6-2d71-45f6-bf51-32f68aa0ac52"
 
